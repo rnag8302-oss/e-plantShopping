89dbea42b28828a43c2add19fd8317e10d71791d
Rohit Nag <rnag8302@gmail.com>
2026-07-05 07:21:15 -0400

    Completed Task 2

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -7,16 +7,36 @@ export const CartSlice = createSlice({
   },
   reducers: {
     addItem: (state, action) => {
-      state.items.push({
-        ...action.payload,
-        quantity: 1,
-      });
+      const item = state.items.find(
+        (item) => item.name === action.payload.name
+      );
+  
+      if (item) {
+        item.quantity += 1;
+      } else {
+        state.items.push({
+          ...action.payload,
+          quantity: 1,
+        });
+      }
     },
-
+  
     removeItem: (state, action) => {
+      state.items = state.items.filter(
+        (item) => item.name !== action.payload
+      );
     },
-
+  
     updateQuantity: (state, action) => {
+      const { name, amount } = action.payload;
+  
+      const item = state.items.find(
+        (item) => item.name === name
+      );
+  
+      if (item) {
+        item.quantity = amount;
+      }
     },
   },
 });
